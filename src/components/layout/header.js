21477e5e7dbd241e2e523f492a013b9419d8a9f6
Bjay <jayabjay2018@gmail.com>
2023-05-15 09:52:01 +0300

    Installed dependancies/Cleaned up The App

---
+++ src/components/layout/header.js
@@ -0,0 +1,13 @@
+import SearchIcon from '@mui/icons-material/Search';
+import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
+import { Link } from 'react-router-dom';
+
+const header = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default header

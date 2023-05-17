a02bc74f9ce0ccd8c30ea3e7b9dbb35a85f7ad4a
Bjay <jayabjay2018@gmail.com>
2023-05-17 11:18:41 +0300

    Implementing routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
 import Home from './components/Home/Home';
 import Header from './components/layout/Header';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
     <div className="App">
        <Header/>
-       <Home/>
+          <main>
+            <Routes>
+              <Route path='/' element={<Home/>}/>
+            </Routes>              
+          </main>
+       <Footer/>
     </div>
   );
 }

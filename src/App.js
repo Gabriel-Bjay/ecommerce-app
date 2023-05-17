0ac7d987d7fa3d99a9762331748a76402f24b894
Bjay <jayabjay2018@gmail.com>
2023-05-17 11:35:02 +0300

    Implementing routing

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import { Route, Routes } from 'react-router-dom';
 import Home from './components/Home/Home';
 import Header from './components/layout/Header';
 import Footer from './components/Footer/Footer';
+import Login from "./components/Login/Login";
+import Categories from './components/Categories/Categories';
+import Ideas from "./components/ideas/Ideas";
 
 function App() {
   return (
@@ -11,6 +14,9 @@ function App() {
           <main>
             <Routes>
               <Route path='/' element={<Home/>}/>
+              <Route path='login' element={<Login/>}/>
+              <Route path='categories' element={<Categories/>}/>
+              <Route path='ideas' element={<Ideas/>}/>
             </Routes>              
           </main>
        <Footer/>

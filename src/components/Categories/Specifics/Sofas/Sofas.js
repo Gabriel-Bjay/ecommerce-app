fb6f2a929ba24375a057370274abc1c9669fd4f3
Bjay <jayabjay2018@gmail.com>
2023-05-24 11:50:39 +0300

    Updates

--- src/components/Categories/Specifics/Sofas/Sofas.js
+++ src/components/Categories/Specifics/Sofas/Sofas.js
@@ -1,74 +1,19 @@
-// import React from 'react'
-// import { ShoppingBasket } from '@mui/icons-material'
-// import "./Sofas.css"
-
-// const Sofas = () => {
-//   return (
-//     <div className='sofas-container'>
-//     <div className='sofa'>
-//       <img src='https://neolivin.com/wp-content/uploads/2022/06/25-1-2-768x768-optimized.png' alt='' className='sofa-image'/>
-//       <p className='sofa-text'>Aston Sofa</p>
-//       <p className='sofa-text'>$ 624.76</p>
-//       <ShoppingBasket/>
-//     </div>
-//     <div className='sofa'>
-//       <img src='https://neolivin.com/wp-content/uploads/2022/08/92-768x768-optimized.png' alt='' className='sofa-image'/>   
-//       <p className='sofa-text'>Tommy Sofa 1-Seater</p>
-//       <p className='sofa-text'>$ 1,430</p>
-//       <ShoppingBasket/>
-//     </div>
-//     <div className='sofa'>
-//       <img src='https://neolivin.com/wp-content/uploads/2022/11/9-768x768-optimized.jpg' alt='' className='sofa-image'/>
-//       <p className='sofa-text'>Franco sofa 3-seater</p>
-//       <p className='sofa-text'>$ 1,300</p>
-//       <ShoppingBasket/>
-//     </div>
-//     <div className='sofa'>
-//       <img src='https://neolivin.com/wp-content/uploads/2022/11/71-768x768-optimized.png' alt='' className='sofa-image'/>
-//       <p className='sofa-text'>Starex Sofa</p>
-//       <p className='sofa-text'>$ 1,000</p>
-//       <ShoppingBasket/>
-//     </div>
-//     <div className='sofa'>
-//       <img src='https://neolivin.com/wp-content/uploads/2023/03/11-4-768x768-optimized.jpg' alt='' className='sofa-image'/>
-//       <p className='sofa-text'>Dzire Sofa</p>
-//       <p className='sofa-text'>$ 240</p>
-//       <ShoppingBasket/>
-//     </div>
-//     <div className='sofa'>
-//       <img src='https://neolivin.com/wp-content/uploads/2022/06/71-4-768x768-optimized.png' alt='' className='sofa-image'/>
-//       <p className='sofa-text'>Ceni Sofa 3 Seater</p>
-//       <p className='sofa-text'>$ 1,200</p>
-//       <ShoppingBasket/>
-//     </div>
-//     <div className='sofa'>
-//       <img src='https://neolivin.com/wp-content/uploads/2022/09/20-2-768x768-optimized.jpg' alt='' className='sofa-image'/>
-//       <p className='sofa-text'>Sienta Sofa 3 -seater</p>
-//       <p className='sofa-text'>$ 800</p>
-//       <ShoppingBasket/>
-//     </div>
-//     <div className='sofa'>
-//       <img src='https://neolivin.com/wp-content/uploads/2022/09/Untitled-design-84-768x768-optimized.png' alt='' className='sofa-image'/>
-//       <p className='sofa-text'>Tucson Sofa 1 seater</p>
-//       <p className='sofa-text'>$ 970</p>
-//       <ShoppingBasket/>
-//     </div>
-//   </div>
-  
-//   )
-// }
-
-// export default Sofas
-
-
-// Sofas.js
-
-import React, {useContext} from 'react';
+import React, {useContext, useState} from 'react';
 import productsData from '../../../../products.json';
 import './Sofas.css'
+import productDetails from '../../../../productDetails.json';
 import CartContext from '../../../../context/CartContext';
 
 function Sofas({ id, image, title, price }) {
+    const [selectedSofa, setSelectedSofa] = useState(null);
+
+  const selectSofa = (sofaId) => {
+    setSelectedSofa(sofaId);
+  };
+
+  const closeModal = () => {
+    setSelectedSofa(null);
+  };
   // Filter the products based on the category "Sofas"
   const sofasData = productsData.filter((product) => product.category === 'Sofas');
   const cartContext = useContext(CartContext)
@@ -87,7 +32,7 @@ function Sofas({ id, image, title, price }) {
       <div className='sofas-container'>
         {sofasData.map((sofa) => (
           <div key={sofa.id} className='sofa'>
-            <img src={sofa.image} alt={sofa.name} className='sofa-image'/>
+            <img src={sofa.image} alt={sofa.name} className='sofa-image' onClick={()=>selectSofa(sofa.id)} />
             <h3 className='sofa-name'>{sofa.name}</h3>
             <p className='price-tag'>Price: {sofa.price}</p>
             <div className='rating'>
@@ -98,6 +43,23 @@ function Sofas({ id, image, title, price }) {
           </div>
         ))}
       </div>
+
+      {selectedSofa !== null && (
+        <div className='modal-overlay' onClick={closeModal}>
+          <div className='modal' onClick={(e) => e.stopPropagation()}>
+            {productDetails
+              .filter((sofa) => sofa.id === selectedSofa)
+              .map((sofa) => (
+                <div key={sofa.id}>
+                  <img src={sofa.image} alt={sofa.name} />
+                  <p>Price: ${sofa.price}</p>
+                  <p>Description: {sofa.description}</p>
+                  {/* Render other relevant details */}
+                </div>
+              ))}
+          </div>
+        </div>
+      )}
     </>
   );
 }

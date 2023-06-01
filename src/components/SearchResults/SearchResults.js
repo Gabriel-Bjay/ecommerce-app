21a4b9b593a4f5218deec93070ad6aa589098b34
Bjay <jayabjay2018@gmail.com>
2023-06-01 12:15:23 +0300

    Updates

--- src/components/SearchResults/SearchResults.js
+++ src/components/SearchResults/SearchResults.js
@@ -1,21 +1,16 @@
+import React from 'react';
 import { Link } from 'react-router-dom';
 import data from '../../products.json';
 
-const SearchResults = () => {
+const SearchResults = ({ category }) => {
   // Filter the items based on the selected category
-  const filteredItems = data.filter(
-    (item) =>
-      item.category === 'Chairs' ||
-      item.category === 'Beds' ||
-      item.category === 'Tables' ||
-      item.category === 'Sofas'
-  );
+  const filteredItems = data.filter((item) => item.category === category);
 
   console.log('Filtered Items:', filteredItems); // Log the filtered items
 
   return (
     <div>
-      <h2>Search Results for Category: Chairs, Beds, Tables, Sofas</h2>
+      <h2>Search Results for Category: {category}</h2>
       <ul>
         {filteredItems.map((item) => (
           <li key={item.id}>

6b487197d2827dbf97e89b3f0ee302cd46150994
Bjay <jayabjay2018@gmail.com>
2023-06-05 07:14:52 +0300

    Updates to the search items display

--- src/components/layout/SearchItems.js
+++ src/components/layout/SearchItems.js
@@ -1,22 +1,25 @@
 import React from 'react'
+import './SearchItems.css'
 
-const SearchItems = ({filteredData}) => {
+const SearchItems = ({searchData}) => {
   return (
-    <div>
-      <h1>Items Page</h1>
-      <div className="item-container">
-      {filteredData.map((val, key) => {
-        return (
-          <div key={key}>
-            <img src={val.image} alt={val.name} />
-            <p>{val.name}</p>
-            <p>{val.price}</p>
-            <p>{val.rating}</p>
-          </div>
-        );
-      })}
+    <>
+      <h1 className='search-header'>Items Page</h1>
+      <div className='search-items'>
+        
+          {searchData.map((val, key) => {
+            return (
+              <div key={key} className='item'>
+                <img src={val.image} alt={val.name} className='item-image' />
+                <p>{val.name}</p>
+                <p>$ {val.price}</p>
+                <p>{val.rating}</p>
+              </div>
+            );
+          })}
+        
       </div>
-    </div>
+    </>
   )
 }
 
